1f48d029a454ee38fc20607e4e241ee3162b11e2
Chinchinooo <chinchin081298@gmail.com>
2024-04-12 23:51:04 +0800

    added: - navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation/>
     </div>
   );
 }
