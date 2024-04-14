6edac63a7399f9686a837672307b15348d552ba0
Chinchinooo <chinchin081298@gmail.com>
2024-04-14 17:08:35 +0800

    fixed: -card takes full length so I can wrap as side bar of cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Navigation from './components/Navigation/Navigation';
 import './App.css';
+import Navigation from './components/Navigation/Navigation.jsx';
+import Card from './components/Card/Card.jsx';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Card/>
     </div>
   );
 }
