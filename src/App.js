153d0061a4f2c8f6f6970f908b313f5400ed376f
Chinchinooo <chinchin081298@gmail.com>
2024-04-15 16:55:29 +0800

    update: -fixing dashboard layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation.jsx';
 import Card from './components/Card/Card.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
-      <Card/>
+      <Dashboard/>
+      
     </div>
   );
 }
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Dashboard = () => {
+    return(
+        <div>
+            {/* SideBar */}
+            <div class="flex">
+                <div class="w-1/2">
+                    Hi
+                </div>
+                <div class="w-1/2">
+                    Hi
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
