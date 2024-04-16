dd0c4c6660240a4fc1eb89f419c38bf75c5106a0
Chinchinooo <chinchin081298@gmail.com>
2024-04-16 18:15:03 +0800

    added: - another card

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import '../../index.css'
+import Card from "../Card/Card";
 
 const Dashboard = () => {
     return(
         <div>
             {/* SideBar */}
             <div class="flex">
-                <div class="w-1/2">
-                    Hi
+                <div class="ml-2 md:basis-1 lg:basis-1/3">
+                    <Card/>
+                    <Card/>
                 </div>
-                <div class="w-1/2">
-                    Hi
+                <div class="">
+                    {/* <Entry/> */}
                 </div>
             </div>
         </div>
