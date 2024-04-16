8623e2122dd6833dd6e5d2434c29e64ecb9eac45
Chinchinooo <chinchin081298@gmail.com>
2024-04-16 18:14:37 +0800

    update: -added margin bottom

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div class=''>
+    <div>
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
-        <div class="bg-white shadow-md  rounded-3xl p-4">
+        <div class="bg-white shadow-md mb-3 rounded-3xl p-4">
                 <div class="flex-none lg:flex">
                     <div class=" h-full w-full lg:h-48 lg:w-48   lg:mb-0 mb-3">
                         <img src="https://images.unsplash.com/photo-1622180203374-9524a54b734d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
