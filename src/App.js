7255ee80d605eb327f692a7001b3a611cfe38f00
Chinchinooo <chinchin081298@gmail.com>
2024-04-18 18:35:43 +0800

    comments on how to start function

--- src/App.js
+++ src/App.js
@@ -14,3 +14,31 @@ function App() {
 }
 
 export default App;
+
+/* 
+Nav
+  Create button
+    - add new card
+    - add image/emotion/text
+    - save button
+  Home button
+    - redirect to dashboard
+  Calendar button
+    - track on calendar whether entry been made with emoji (can see the emoji)
+    - select on the date can show the entry (next step)
+
+Card
+  View Diary
+    - active - card === entry
+  Edit Diary
+    -active - edit card through entry
+  Time and location
+    -show time and location after save
+
+Do 
+  Create button
+    - pop add new entry with emotion/title/date/location/image
+    - add new card
+    - add image/emotion/text
+    - save button
+*/
