9a2b3b9926118bacb7783a96aefc844812e67961
Chinchinooo <chinchin081298@gmail.com>
2024-04-19 15:55:39 +0800

    added: -fake api to test card

---
+++ src/api.js
@@ -0,0 +1,46 @@
+export const api = [
+    {
+        id: 1,
+        emotion: 'Happy',
+        title: 'I nailed the job interview!',
+        time: '01-01-2024',
+        location: 'Semporna'
+    },
+    {
+        id: 2,
+        emotion: 'Nervous',
+        title: 'I havent heard from the company...',
+        time: '02-01-2024',
+        location: 'Semporna'
+    },
+    {
+        id: 3,
+        emotion: 'Sad',
+        title: 'I guess I failed the interview',
+        time: '03-01-2024',
+        location: 'Semporna'
+    },
+    {
+        id: 4,
+        emotion: 'Happy',
+        title: 'They just called!',
+        time: '04-01-2024',
+        location: 'Semporna'
+    },
+    {
+        id: 5,
+        emotion: 'Happy',
+        title: 'I got the job!',
+        time: '05-01-2024',
+        location: 'Semporna'
+    },
+    {
+        id: 6,
+        emotion: 'Excited',
+        title: 'First day in!',
+        time: '06-01-2024',
+        location: 'Semporna'
+    },    
+    
+
+]
