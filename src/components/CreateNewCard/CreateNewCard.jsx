43ece216c7ceccd6cffcddfedb07888e52350ef7
Chinchinooo <chinchin081298@gmail.com>
2024-04-28 18:09:45 +0800

    updated: - change emotion, title, location to <input>
    - text hover over title shown when too long it turns "..."
    
    - date automatically set to that day

--- src/components/CreateNewCard/CreateNewCard.jsx
+++ src/components/CreateNewCard/CreateNewCard.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function getDate() {
   const today = new Date();
@@ -11,62 +11,107 @@ function getDate() {
   return `${paddedDay}-${paddedMonth}-${year}`;
 }
 
-
 function CreateNewCard() {
     const currentDate = getDate();
+    const [title, setTitle] = useState("");
+
+    const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+} 
 
     return(
-         <div class="h-screen w-screen flex items-center justify-center">
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
-        <div class="bg-white shadow-md mb-2 rounded-3xl p-4 w-6/12">
-                <div class="flex-none lg:flex">
-                    <div class=" h-full w-full lg:h-48 lg:w-48   lg:mb-0 mb-3">
-                        <img src="https://images.unsplash.com/photo-1622180203374-9524a54b734d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
-                            alt="Just a flower" class=" w-full  object-scale-down lg:object-cover  lg:h-48 rounded-2xl"/>
-                    </div>
-                    <div class="flex-auto ml-3 justify-center items-center  py-2">
-                        <div class="flex flex-wrap ">
-                            <div class="w-full flex-none text-xs text-blue-700 font-medium ">
-                                <input type="text" placeholder="How are you feeling?">
-                                </input>
-                            </div>
-                            <div class="flex-auto text-lg font-medium">
-                                <input type="text" placeholder="Title"></input>
-                            </div>
-
-                        </div>
-                        <p class="mt-3"></p>
-                        <div class="flex start- py-4  text-sm text-gray-500">
-                            <div class="flex-1 inline-flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3 text-gray-400" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z">
-                                    </path>
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                </svg>
-                                <input type="text" placeholder="location"></input>
-                            </div>
-                            <div class="flex-1 inline-flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-gray-400" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                </svg>
-                                <p class="">{currentDate}</p>
-                            </div>
-                        </div>
-                        <button
-                                class=" bg-gray-900 px-4 py-2 shadow-sm tracking-wider text-white rounded-full hover:bg-gray-800"
-                                type="button"
-                                aria-label="like"
-                                >Create Diary
-                        </button>
-                        </div>
-                    </div>
-                </div>
+   <div className="min-w-screen h-screen fixed left-0 top-0 flex justify-center items-center inset-0 z-50 bg-no-repeat bg-center bg-cover">
+  <div className="absolute bg-black opacity-80 inset-0 z-0"></div>
+  <div className="relative flex flex-col items-center justify-center p-5 w-full min-h-screen lg:w-8/12 xl:w-6/12 2xl:w-4/12 2xl:min-h-full"> 
+    <div className="flex flex-col">
+      <div className="bg-white shadow-md rounded-3xl p-4 w-full h-auto 2xl:p-8 2xl:w-full 2xl:h-auto">
+        <div className="flex-none lg:flex">
+          <div className="h-full w-full lg:h-48 lg:w-48 lg:mb-0 mb-3">
+            <img
+              src="https://images.unsplash.com/photo-1585399000684-d2f72660f092"
+              alt="Just a flower"
+              className="w-full object-cover lg:h-48 lg:w-48 rounded-2xl"
+            />
+          </div>
+          <div className="flex-auto ml-3 justify-evenly py-2">
+            <div className="flex flex-wrap">
+              <div className="w-full flex-none text-xs text-blue-700 font-medium">
+                <div className="flex-auto text-lg font-medium">
+                <input
+                type="text" 
+                placeholder="How are you feeling?"
+                className="text-center text-sm text-blue-700"/>
+              </div>
+              <div className="m-2"></div>
+              </div>
+              <div className="flex-auto text-lg font-medium">
+                <input
+                type="text" 
+                placeholder="Title for your Diary"
+                className="text-center overflow-hidden text-ellipsis whitespace-nowrap"
+                style={{ maxWidth: "200px" }}
+                value={title} 
+                onChange={handleTitleChange} 
+                title={title}/>
+              </div>
             </div>
+            <div className="flex py-4 text-sm text-gray-500">
+              <div className="flex-1 inline-flex items-center">
+                 <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5 mr-3 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24" 
+                    stroke="currentColor">
+                        <path 
+                            stroke-linecap="round" 
+                            stroke-linejoin="round" 
+                            stroke-width="2"
+                            d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z">
+                        </path>
+                        <path 
+                            stroke-linecap="round" 
+                            stroke-linejoin="round" 
+                            stroke-width="2"
+                            d="M15 11a3 3 0 11-6 0 3 3 0 016 0z">
+                        </path>
+                </svg>
+                <input
+                type="text" 
+                placeholder="location"
+                className=" text-sm"/>
+              </div>
+              <div className="inline-flex items-end pl-1">
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    class="h-5 w-5 mr-2 text-gray-400" 
+                    fill="none"
+                    viewBox="0 0 24 24" 
+                    stroke="currentColor">
+                        <path 
+                            stroke-linecap="round" 
+                            stroke-linejoin="round" 
+                            stroke-width="2"
+                            d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z">
+                        </path>
+                </svg>
+                <p>{currentDate}</p>
+              </div>
+            </div>
+            <div className="flex p-4 border-t border-gray-200"></div>
+            <div className=" text-sm font-medium">
+              <button
+                className="mb-2 md:mb-0 bg-gray-900 px-5 py-2 shadow-sm tracking-wider text-white rounded-full hover:bg-gray-800"
+                >Save Diary
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
     )
 }
 
